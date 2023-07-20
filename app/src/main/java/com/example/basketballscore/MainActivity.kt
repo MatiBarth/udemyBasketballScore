8f2255596591a4c70de5d19f95474f94cc2e331d
Matias Barthelemi <mbarthelemi@prismamp.com>
2023-07-20 08:11:17 -0300

    app terminada

--- app/src/main/java/com/example/basketballscore/MainActivity.kt
+++ app/src/main/java/com/example/basketballscore/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.basketballscore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.basketballscore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var localScore: Int = binding.localBoard.text.toString().toInt()
+        var visitorScore: Int = binding.visitorBoard.text.toString().toInt()
+
+        binding.localButtonToSubtractPoint.setOnClickListener {
+            localScore = localScore - 1
+            if (localScore <= 0) localScore = 0
+            binding.localBoard.text = localScore.toString()
+        }
+
+        binding.localButtonToAddOnePoint.setOnClickListener {
+            localScore++
+            binding.localBoard.text = localScore.toString()
+        }
+
+        binding.localButtonToAddTwoPoints.setOnClickListener {
+            localScore = localScore + 2
+            binding.localBoard.text = localScore.toString()
+        }
+
+        binding.visitorButtonToSubtractPoint.setOnClickListener {
+            visitorScore = visitorScore - 1
+            if (visitorScore <= 0) visitorScore = 0
+            binding.visitorBoard.text = visitorScore.toString()
+        }
+
+        binding.visitorButtonToAddOnePoint.setOnClickListener {
+            visitorScore++
+            binding.visitorBoard.text = visitorScore.toString()
+        }
+
+        binding.visitorButtonToAddTwoPoints.setOnClickListener {
+            visitorScore = visitorScore + 2
+            binding.visitorBoard.text = visitorScore.toString()
+        }
+
+        binding.buttonRestartPoints.setOnClickListener{
+            visitorScore = 0
+            localScore = 0
+            binding.visitorBoard.text = visitorScore.toString()
+            binding.localBoard.text = localScore.toString()
+        }
+        binding.buttonToScoreActivity.setOnClickListener {
+            val intent = Intent(this, ScoreActivity::class.java)
+            intent.putExtra(ScoreActivity.LOCAL_SCORE_KEY, localScore)
+            intent.putExtra(ScoreActivity.VISITOR_SCORE_KEY, visitorScore)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/basketballscore/ScoreActivity.kt
+++ app/src/main/java/com/example/basketballscore/ScoreActivity.kt
@@ -2,10 +2,34 @@ package com.example.basketballscore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.basketballscore.databinding.ActivityMainBinding
+import com.example.basketballscore.databinding.ActivityScoreBinding
 
 class ScoreActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityScoreBinding
+    private var localScore: Int = 0
+    private var visitorScore: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_score)
+        binding = ActivityScoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        localScore = intent.getIntExtra(LOCAL_SCORE_KEY,0)
+        visitorScore = intent.getIntExtra(VISITOR_SCORE_KEY,0)
+
+        binding.scoreResult.text = localScore.toString() + " - " + visitorScore.toString()
+        finalResult()
+    }
+
+    private fun finalResult() {
+        if(localScore>visitorScore) binding.finalResult.text = getString(R.string.string_victoria,"local")
+        else if(localScore<visitorScore) binding.finalResult.text = getString(R.string.string_victoria,"visitante")
+        else binding.finalResult.text = getString(R.string.string_empate)
+    }
+
+    companion object{
+        const val LOCAL_SCORE_KEY = "localScore"
+        const val VISITOR_SCORE_KEY = "visitorScore"
     }
 }
